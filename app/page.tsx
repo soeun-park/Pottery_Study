ea86b778231c53b314773c0c3c8af69c374a5f4b
soeun-park <52274054+soeun-park@users.noreply.github.com>
2023-08-11 22:09:27 +0900

    Update MainPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
 import Together1 from 'img/together1.png'
+import Together2 from 'img/together2.png'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col">
       <div className="w-full">
         <div className="flex justify-between items-center mx-[82px] py-[10px] border-b border-black ">
           <div className="flex space-x-[45px] items-center">
@@ -38,7 +39,22 @@ export default function Home() {
             <button className="w-[125px] py-[10px] bg-white rounded-[10px]">구입하기</button>
             <button className="text-white" >더 알아보기 &#62;</button>
           </div>
-
+        </div>
+      </div>
+      <div className="relative">
+        <Image src={Together2} alt="" />
+        <div className="absolute top-0 bottom-0 flex-col flex justify-center px-[82px]">
+          <div className="flex space-x-[8px] text-white">
+            <div>캠페인</div>
+            <div>|</div>
+            <div>여름 23</div>
+          </div>
+          <div className="text-white font-medium text-[45px] leading-[55px] pt-[12px]">포터리만의 유려한<br />
+            실루엣과 편안한 착용감</div>
+          <div className="flex space-x-[30px] pt-[32px]">
+            <button className="w-[125px] py-[10px] bg-white rounded-[10px]">구입하기</button>
+            <button className="text-white" >더 알아보기 &#62;</button>
+          </div>
         </div>
       </div>
     </main>
